6ff91fe476473ef0e3a4a9021452f045f26134fd
mastanpatel <m.mastanpatel@gmail.com>
2020-04-15 14:55:35 +0530

    added the login component

---
+++ src/app/portal-login/portal-login.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-portal-login',
+  templateUrl: './portal-login.component.html',
+  styleUrls: ['./portal-login.component.css']
+})
+export class PortalLoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
